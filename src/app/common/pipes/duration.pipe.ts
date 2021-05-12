eb687644e24c1726f63585c7946a122845a1e3ee
BREAUR Eugen <ebreaur@gmail.com>
2021-05-12 08:14:13 +0300

    Styled time column

--- src/app/common/pipes/duration.pipe.ts
+++ src/app/common/pipes/duration.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationPipe implements PipeTransform {
   transform(value: number): string {
     if (value % 60) {
-        return Math.floor(value / 60) + ' h-' + (value % 60) + ' m';
+        return Math.floor(value / 60) + 'h:' + (value % 60) + 'm';
     } else {
-        return Math.floor(value / 60) + ' h';
+        return Math.floor(value / 60) + 'h';
     }
   }
 }
